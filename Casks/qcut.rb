2244ead68c929d439de75f9fb1a803674ff8fd03
虚無 <ieee0824x@gmail.com>
2026-03-10 13:20:55 +0900

    qcut 0.1.0 正式版を追加

---
+++ Casks/qcut.rb
@@ -0,0 +1,20 @@
+cask "qcut" do
+  version "0.1.0"
+  sha256 "be42c99b208b3da71c1f26c5dbcca4e5ba3a24b7cf4f49d6fd17a963c1281329"
+
+  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-darwin-aarch64.dmg"
+  name "qcut"
+  desc "Cross-platform video editor"
+  homepage "https://github.com/ieee0824/qcut"
+
+  depends_on arch: :arm64
+
+  app "qcut.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/qcut.app"]
+  end
+
+  conflicts_with cask: "qcut-beta"
+end
