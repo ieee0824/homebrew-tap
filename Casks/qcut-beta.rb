db5e2a9bb3104c17fdb906fdbbd9f7d887ed28d8
虛無 <ieee0824x@gmail.com>
2026-03-13 22:28:09 +0900

    cask の DMG URL をバージョン付きファイル名に修正
    理由:
    - v0.2.1 からアーティファクト名が qcut-v{version}-darwin-aarch64.dmg に変更されたため
    
    変更内容:
    - qcut.rb, qcut-beta.rb の url を qcut-v#{version}-darwin-aarch64.dmg に更新

--- Casks/qcut-beta.rb
+++ Casks/qcut-beta.rb
@@ -2,7 +2,7 @@ cask "qcut-beta" do
   version "0.2.1-beta.1"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-darwin-aarch64.dmg"
+  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-v#{version}-darwin-aarch64.dmg"
   name "qcut (Beta)"
   desc "Cross-platform video editor"
   homepage "https://github.com/ieee0824/qcut"
--- Casks/qcut.rb
+++ Casks/qcut.rb
@@ -2,7 +2,7 @@ cask "qcut" do
   version "0.2.1"
   sha256 "74f27c4b1cf22ef0850288bb2c34ef14be4320327be90e295f7033a1d578e4f9"
 
-  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-darwin-aarch64.dmg"
+  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-v#{version}-darwin-aarch64.dmg"
   name "qcut"
   desc "Cross-platform video editor"
   homepage "https://github.com/ieee0824/qcut"
