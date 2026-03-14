39c2aea89b0239785cd20be08a85ea9d613b283c
虛無 <ieee0824x@gmail.com>
2026-03-14 18:30:14 +0900

    qcut-beta cask の URL パスを修正

--- Casks/qcut-beta.rb
+++ Casks/qcut-beta.rb
@@ -2,7 +2,7 @@ cask "qcut-beta" do
   version "0.3.0-beta.1"
   sha256 "35753b2b16d8690043bd5343d4d38091ac49f30bc801754525a2eba29666b0a8"
 
-  url "https://github.com/ieee0824/qcut-v#{version}-darwin-aarch64.dmg"
+  url "https://github.com/ieee0824/qcut/releases/download/v#{version}/qcut-v#{version}-darwin-aarch64.dmg"
   name "qcut (Beta)"
   desc "Cross-platform video editor"
   homepage "https://github.com/ieee0824/qcut"
